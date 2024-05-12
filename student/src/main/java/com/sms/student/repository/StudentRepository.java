5e4120c6d88d58da36802419c3c11af24fcec8ae
parindia <parindia@gmail.com>
2024-05-12 12:28:59 +0800

    added html and method
    added html and method

--- student/src/main/java/com/sms/student/repository/StudentRepository.java
+++ student/src/main/java/com/sms/student/repository/StudentRepository.java
@@ -4,4 +4,7 @@ import com.sms.student.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository <Student, Long>{
+
+    @Query("SELECT u FROM Student u WHERE u.name = ?1")
+    Employee findStudentByName(String name);
 }
